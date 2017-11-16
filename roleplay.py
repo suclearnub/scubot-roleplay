6bac7aa6982b39c8fbd09a77c5a73a1c51229d66
suclearnub <suclearnub00@gmail.com>
2017-11-16 12:38:51 +0800

    Add function for channel-by-channel dates

--- roleplay.py
+++ roleplay.py
@@ -23,11 +23,13 @@ class Roleplay(BotModule):
 
         trigger_string = 'rp'
 
-        next_day_role = ['Literal Emerald'] # Which role is allowed to advance to next day.
+        next_day_role = ['Literal Emerald'] # Which role is allowed to edit dates
 
         date_format = '%Y-%m-%d'
 
         module_version = '0.1.0'
+
+        date_colour = 0xBADA55 # Colour of the date embed
 
         def is_valid_date(self, x):
             try:
@@ -69,7 +71,7 @@ class Roleplay(BotModule):
                                     msg = "[!] You do not have permission to edit this."
                                     await client.send_message(message.channel, msg)
                                 else:
-                                    self.module_db.update({'name': msg[3], 'gender': msg[4], 'species': msg[5], 'status': msg[6], 'age': msg[7], 'height': msg[8], 'weight': msg[9], 'desc': msg[10], 'pic': msg[11], 'author': message.author.name, 'colour': int(msg[12], 16), 'authorid': message.author.id})
+                                    self.module_db.update({'name': msg[3], 'gender': msg[4], 'species': msg[5], 'status': msg[6], 'age': msg[7], 'height': msg[8], 'weight': msg[9], 'desc': msg[10], 'pic': msg[11], 'author': message.author.name, 'colour': int(msg[12], 16), 'authorid': message.author.id}, roleplay_query.name == msg[3])
                                     msg = "[:ok_hand:] Entry updated."
                                     await client.send_message(message.channel, msg)
                         else:
@@ -102,14 +104,14 @@ class Roleplay(BotModule):
                         if msg[2] == 'edit':
                             if any(i in author_roles for i in self.next_day_role):
                                 if self.is_valid_date(msg[3]):
-                                    if len(self.module_db) == 0:
+                                    if self.module_db.get(roleplay_query.channel == message.channel.id) is None:
                                         # There is no date set up yet.
-                                        self.module_db.insert({'date': msg[3], 'date_actual': int(time.time()), 'last_edit': message.author.name})
+                                        self.module_db.insert({'date': msg[3], 'date_actual': int(time.time()), 'last_edit': message.author.name, 'channel': message.channel.id})
                                         msg = "[:ok_hand:] Date has been successfully set."
                                         await client.send_message(message.channel, msg)
                                     else:
                                         # There is a date!
-                                        self.module_db.update({'date': msg[3], 'date_actual': int(time.time()), 'last_edit': message.author.name})
+                                        self.module_db.update({'date': msg[3], 'date_actual': int(time.time()), 'last_edit': message.author.name, 'channel': message.channel.id}, roleplay_query.channel == message.channel.id)
                                         msg = "[:ok_hand:] Date has been successfully edited."
                                         await client.send_message(message.channel, msg)
                                 else:
@@ -120,20 +122,24 @@ class Roleplay(BotModule):
                                 await client.send_message(message.channel, msg)
                         elif msg[2] == '+':
                             if any(i in author_roles for i in self.next_day_role):
-                                new_day = datetime.strptime(self.module_db.all()[0]['date'], self.date_format) + timedelta(days=1)
-                                self.module_db.update({'date': datetime.strftime(new_day, self.date_format), 'date_actual': int(time.time()), 'last_edit': message.author.name})
-                                msg = "[:ok_hand:] Date set to " + datetime.strftime(new_day, self.date_format) + "."
+                                new_day = datetime.strptime(self.module_db.get(roleplay_query.channel == message.channel.id)['date'], self.date_format) + timedelta(days=1)
+                                self.module_db.update({'date': datetime.strftime(new_day, self.date_format), 'date_actual': int(time.time()), 'last_edit': message.author.name, 'channel': message.channel.id}, roleplay_query.channel == message.channel.id)
+                                msg = "[:ok_hand:] Date incremented to " + datetime.strftime(new_day, self.date_format) + "."
                                 await client.send_message(message.channel, msg)
                             else:
                                 msg = "[!] You do not have permission to advance the time."
                                 await client.send_message(message.channel, msg)
                     else:
                         # It can only be !rp day
-                        date_info = self.module_db.all()[0]
-                        text = 'Last changed: ' + datetime.fromtimestamp(date_info['date_actual']).strftime(self.date_format + ' %X') + ' GMT \n' \
-                               'Edited by: ' + date_info['last_edit'] + '\n'
-                        embed = discord.Embed(title=date_info['date'], description=text, colour=0xBADA55)
-                        await client.send_message(message.channel, embed=embed)
+                        if self.module_db.get(roleplay_query.channel == message.channel.id) is None:
+                            msg = "[!] No date set for this channel."
+                            await client.send_message(message.channel, msg)
+                        else:
+                            date_info = self.module_db.get(roleplay_query.channel == message.channel.id)
+                            text = 'Last changed: ' + datetime.fromtimestamp(date_info['date_actual']).strftime(self.date_format + ' %X') + ' GMT \n' \
+                                   'Edited by: ' + date_info['last_edit'] + '\n'
+                            embed = discord.Embed(title=date_info['date'], description=text, colour=self.date_colour)
+                            await client.send_message(message.channel, embed=embed)
                 elif msg[1] == 'setting':
                     table = self.module_db.table('setting')
                     # TODO: Change setting of roleplay scene
